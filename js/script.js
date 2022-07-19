e27e843b1fa1d3d32250857c9d71b2d4fc565b37
Su-jh <91510145+Su-jh@users.noreply.github.com>
2022-07-19 20:02:49 +0800

    20220719

---
+++ js/script.js
@@ -0,0 +1,9 @@
+ <script>
+   var mdBut = document.getElementById('mdbut');
+   var bgcb = document.querySelector('.bgc-rgba07');
+   var beside = document.querySelector('.beside-menu');
+
+   mdBut.onclick = function(){
+        bgcb.classList.add('bgcopen')
+    }
+</script>
